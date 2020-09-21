5956c14236427a8470ebac9a16e655e5e609f7e2
Conor Greene <cgreene1@trinity.edu>
2020-09-21 13:45:29 -0500

    First Commit, got baskets, apples, tree is acting weird tho

---
+++ Assets/BasketBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 convMouse = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector3 pos = transform.position;
+        pos.x = convMouse.x;
+        transform.position = pos;
+    }
+}
---
+++ Assets/TreeBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeBehaviour : MonoBehaviour
+{
+    public GameObject applePrefab;
+    public float treeSpeed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("AppleDropper", 2.0f);
+    }
+
+    void AppleDropper() {
+        GameObject apple = Instantiate <GameObject>(applePrefab);
+
+        apple.transform.position = transform.position;
+
+        Invoke("AppleDropper", 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(treeSpeed * Time.deltaTime, 0, 0);
+       /*if (Mathf.Abs(transform.position.x) > 5.0f) {
+            treeSpeed *= -1.0f;
+        }*/
+    }
+    public void OnCollisionEnter2D(Collision2D coll) {
+        if(coll.gameObject.name == "ScreenBoundaries") {
+            treeSpeed *= -1.0f;
+        }
+    }
+}
